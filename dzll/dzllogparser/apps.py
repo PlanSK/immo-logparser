9935e3c651d836ce8b4093e9e0a36f1d657f69ed
PlanSK <plansk@artcore24.ru>
2023-03-05 16:23:50 +0300

    Install Django. Create models. Migration.

---
+++ dzll/dzllogparser/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DzllogparserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dzllogparser'
---
+++ dzll/dzllogparser/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Gamer(models.Model):
+    steam_id = models.IntegerField(verbose_name='SteamID')
+    dayzname = models.CharField(max_length=255, verbose_name='DayZ nickname')
+    dayz_alt_names = models.TextField(verbose_name='Alternate names',
+                                      blank=True)
+
+
+class Car(models.Model):
+    class CarStatus(models.TextChoices):
+        LINKED = 'LINKED', 'Linked'
+        FREE = 'FREE', 'Free'
+        DELETED = 'DELETED', 'Deleted'
+
+    car_id = models.IntegerField(verbose_name='Server CarID')
+    name = models.CharField(max_length=255, verbose_name='Car name')
+    car_type = models.CharField(max_length=255, verbose_name='Car type')
+    position = models.CharField(max_length=255, verbose_name='Coordinates')
+    car_status = models.CharField(
+        max_length=10, choices=CarStatus.choices,
+        default=CarStatus.LINKED, verbose_name='Car Status'
+    )
+
+
+class Event(models.Model):
+    action_time = models.DateTimeField(verbose_name='Action time')
+    gamer = models.ForeignKey(Gamer, on_delete=models.CASCADE,
+                              verbose_name='Gamer')
+    car = models.ForeignKey(Car, on_delete=models.PROTECT, verbose_name='Car')
+    action = models.CharField(max_length=255, verbose_name='Action')
