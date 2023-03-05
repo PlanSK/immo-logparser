66308eb961747bafb87d0657e6b027aca1e00e7f
PlanSK <plansk@artcore24.ru>
2023-03-05 22:29:40 +0300

    Add file analyzing function.

--- dzll/dzllogparser/services/parser.py
+++ dzll/dzllogparser/services/parser.py
@@ -1,9 +1,13 @@
 import datetime
 import re
+import os
+
 from typing import NamedTuple
+from dataclasses import dataclass
+from enum import Enum
 
 
-FILE = '../ImmobilizerLog(2023_3_4_21_0).log'
+FILE = 'ImmobilizerLog(2023_3_4_21_0).log'
 TIMESTAMP_STR = '1677952821'
 
 INIT_TYPE_STR_TRIGGER = 'initialized.'
@@ -23,16 +27,24 @@ class Player(NamedTuple):
     name: str
 
 
-def defenition_input_data(input_str_data: str) -> None:
-    if input_str_data.endswith(INIT_TYPE_STR_TRIGGER):
-        pass
-    elif input_str_data.endswith(DELETE_TYPE_STR_TRIGGER):
-        pass
-    else:
-        pass
+class EventType(Enum):
+    INIT = 1
+    ACTION = 2
+    DELETE = 3
 
 
-def get_player_data(player_data_string: str) -> Player:
+@dataclass
+class Event:
+    event_type: EventType
+    event_date: datetime.datetime
+    action: str
+    player: Player | None
+    car: Car
+
+
+def get_player_data(log_string: str) -> Player:
+    player_data_string = re.search(
+        r'Player\{.*\.\d{6}\}\s', log_string).group(0)
     steam_id = re.search(r'(?<=steam:)[0-9]{17}', player_data_string).group(0)
     name = re.search(r'(?<=name:)(.*)(?=\ssteam:)',
                      player_data_string).group(0)
@@ -60,23 +72,52 @@ def get_action_time(directory_name: str,
     current_timestamp = float(directory_name)
     current_date = datetime.date.fromtimestamp(current_timestamp)
     current_time_string = re.match(
-        r'^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5]?[0-9]', action_str).group(0)
+        r'^([0-1]?[0-9]|2[0-3]):[0-5]?[0-9]:[0-5]?[0-9]', action_str).group(0)
     action_time = datetime.datetime.strptime(
         current_time_string, '%H:%M:%S').time()
     
     return datetime.datetime.combine(current_date, action_time)
 
 
-def parse_action_str(directory_name: str, action_str: str) -> list:
-    action_time = get_action_time(directory_name, action_str)
-    player_data_string = re.search(
-        r'Player\{.*\.\d{6}\}\s', action_str).group(0)
-    current_player = get_player_data(player_data_string)
-    current_car = get_car_data(action_str)
-    action = re.search(r'(?<=\.\d{6}\}\s)(.*)(?=\scar:)', action_str).group(0)
-    return [action_time, current_player, current_car, action]
+def get_action_str(log_string: str) -> str:
+    action_str = re.search(r'(?<=\.\d{6}\}\s)(.*)(?=\scar:)',
+                           log_string).group(0)
+    return action_str
+
+
+def defenition_input_data(dir_name: str, filename: str) -> list:
+    players = dict()
+    cars = dict()
+    events = []
+    file_path = os.path.join(os.getcwd(), filename)
+    with open(file_path, 'r', encoding='utf-8') as file_instance:
+        for number, file_string in enumerate(file_instance):
+            log_string = file_string.strip()
+            player = None
+            try:
+                car = get_car_data(log_string)
+                action_date = get_action_time(dir_name, log_string)
+            except AttributeError:
+                print(f'Line {number} is skipped.')
+                continue
+            cars.update({car.car_id: car})
+            
+            if log_string.endswith(INIT_TYPE_STR_TRIGGER):
+                event_type = EventType.INIT
+                action=INIT_TYPE_STR_TRIGGER
+            elif log_string.endswith(DELETE_TYPE_STR_TRIGGER):
+                event_type = EventType.DELETE
+                action=DELETE_TYPE_STR_TRIGGER
+            else:
+                player = get_player_data(log_string)
+                players.update({player.steam_id: player})
+                event_type = EventType.ACTION
+                action = get_action_str(log_string)
+            events.append(Event(
+                event_type=event_type, event_date=action_date,
+                action=action, player=player, car=car))
+    return [players, cars, events]
 
 
 if __name__ == '__main__':
-    def_str = '22:38:0 Player{name:Plan steam:76561199163269309 pos:2528.438721 257.033264 9583.657227} сел в машину car:<name=(GAZ-59037) type=BTR id=746019054 pos=2528.236328 256.355103 9582.357422 status=[FREE]>'
-    print(parse_action_str(directory_name=TIMESTAMP_STR, action_str=def_str))
+    print(defenition_input_data(dir_name=TIMESTAMP_STR, filename=FILE))
