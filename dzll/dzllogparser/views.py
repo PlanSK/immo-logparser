a16db4aefc8d5c292d9b5762e7946bb38c51be14
PlanSK <plansk@artcore24.ru>
2023-03-08 23:20:36 +0300

    Temporary use view for import into db.

--- dzll/dzllogparser/views.py
+++ dzll/dzllogparser/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from typing import Any
+
 from django.views.generic import TemplateView
 
+from dzllogparser.services.ftp import get_ftp_data
 
 class IndexView(TemplateView):
     template_name = 'dzllogparser/index.html'
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        get_ftp_data()
+        return context
