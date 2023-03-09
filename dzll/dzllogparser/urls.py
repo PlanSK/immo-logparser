0e13aaa9c24d65c0a2598331f84737797bc82511
PlanSK <plansk@artcore24.ru>
2023-03-09 23:53:58 +0300

    Make index page design. Set static.

--- dzll/dzllogparser/urls.py
+++ dzll/dzllogparser/urls.py
@@ -5,5 +5,6 @@ from dzllogparser.views import IndexView
 
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('update/', IndexView.as_view(), name='update_db')
 ]
--- dzll/dzllogparser/views.py
+++ dzll/dzllogparser/views.py
@@ -1,13 +1,21 @@
 from typing import Any
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 from dzllogparser.services.ftp import get_ftp_data
+
 
 class IndexView(TemplateView):
     template_name = 'dzllogparser/index.html'
     
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        get_ftp_data()
         return context
+
+
+class UpdateDbView(RedirectView):
+    pattern_name = 'index'
+
+    def get_redirect_url(self, *args, **kwargs):
+        get_ftp_data()
+        return super().get_redirect_url(*args, **kwargs)
