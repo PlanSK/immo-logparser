379d59331d33a03cdfa814b0f80bee5eedbc3098
PlanSK <skapkanshchikov@gmail.com>
2023-08-05 11:57:36 +0300

    Remaking models structure

--- dzll/dzllogparser/models.py
+++ dzll/dzllogparser/models.py
@@ -5,6 +5,8 @@ from django.urls import reverse_lazy
 class Player(models.Model):
     steam_id = models.CharField(max_length=255, verbose_name='SteamID', 
                                 db_index=True, unique=True)
+    bohemia_id = models.CharField(max_length=255, verbose_name='Bohemia ID',
+                                  blank=True, unique=True)
     dayzname = models.CharField(max_length=255, verbose_name='DayZ nickname')
     dayz_alt_names = models.TextField(verbose_name='Alternate names',
                                       blank=True)
@@ -17,6 +19,16 @@ class Player(models.Model):
         return f'{self.dayzname} ({self.steam_id})'
 
 
+class Location(models.Model):
+    location_datetime = models.DateTimeField(
+        verbose_name='Location Date and Time')
+    coord_x = models.FloatField(verbose_name='X coordinate')
+    coord_y = models.FloatField(verbose_name='Y coordinate')
+    note = models.CharField(max_length=255, verbose_name='Note')
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, blank=True,
+                               null=True, verbose_name='Player')
+
+
 class Car(models.Model):
     class CarStatus(models.TextChoices):
         LINKED = 'LINKED', 'Linked'
@@ -27,7 +39,7 @@ class Car(models.Model):
                               db_index=True)
     name = models.CharField(max_length=255, verbose_name='Car name')
     car_type = models.CharField(max_length=255, verbose_name='Car type')
-    position = models.CharField(max_length=255, verbose_name='Coordinates')
+    position = models.ForeignKey(Location, on_delete=models.PROTECT)
     car_status = models.CharField(
         max_length=10, choices=CarStatus.choices,
         default=CarStatus.LINKED, verbose_name='Car Status'
@@ -53,8 +65,8 @@ class Event(models.Model):
                               verbose_name='Player', blank=True, null=True)
     car = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name='Car')
     action = models.CharField(max_length=255, verbose_name='Action')
-    position = models.CharField(max_length=255,
-                                           verbose_name='Coordinates')
+    position = models.ForeignKey(Location, on_delete=models.PROTECT,
+                                 verbose_name='Location')
 
     def __str__(self) -> str:
         return f'{self.action_time} {self.player} {self.action}'
